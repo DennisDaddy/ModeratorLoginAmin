40e34eca2c20a0030a34e0988e199c24967da42b
DennisDaddy <softteez95@gmail.com>
2017-02-07 11:10:32 +0300

    initial commit

---
+++ app/models/post.rb
@@ -0,0 +1,7 @@
+class Post < ApplicationRecord
+  has_many :comments
+  has_many  :tags, through: :post_tags
+  belongs_to :moderator
+
+end
+
---
+++ app/models/visitor.rb
@@ -0,0 +1,7 @@
+class Visitor < ApplicationRecord
+	has_many :notifications, as: :notifiable
+	has_many :comments
+	has_many :messages
+
+end
+ 
